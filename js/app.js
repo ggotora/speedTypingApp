89f955fc4800d3bbd9699e06380c75b49a817992
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-05-10 22:17:00 +0200

    working here

---
+++ js/app.js
@@ -0,0 +1,98 @@
+const showQuotes = document.querySelector('.show-random-quotes');
+const userInput = document.querySelector('.userInput');
+const showAuthor = document.querySelector('.show-author');
+const showLength = document.querySelector('.show-length');
+const correctInput = document.querySelector('.correct-input')
+const timerSelector = document.querySelector('.timer');
+const results = document.querySelector('.results');
+
+const timerBtnStart = document.querySelector('.starter');
+const btnPause = document.querySelector('.pause')
+const next = document.querySelector('.next');
+
+const showCounter = document.querySelector('.show-counter');
+
+const URL = 'http://api.quotable.io/random';
+function getQuotes(){
+    return fetch('http://api.quotable.io/random')
+    .then(res => res.json())
+    .then(data => data)
+}
+
+async function displayQuotes(){
+    const data = await getQuotes();
+    const author = data.author;
+    const length = data.length;
+    const quote = data.content
+    
+    //display data
+    showQuotes.textContent = quote;
+    showAuthor.textContent = author;
+    showLength.textContent = length;
+    showQuotes.textContent = ''    
+    quote.split('').forEach(character => {
+        const spanToAddChars = document.createElement('span');
+        spanToAddChars.textContent = character;
+        showQuotes.appendChild(spanToAddChars);
+
+    })
+
+}
+
+let correct = 0;
+userInput.addEventListener('input', () =>{
+    //put every letter into a span tag 
+    const quotesSpans = showQuotes.querySelectorAll('span');
+    //get user input values 
+    const valueArray = userInput.value.split(''); 
+    
+ //to handle next challenge
+    let passed = true;
+    quotesSpans.forEach((characterSpan, index) => {
+        const character = valueArray[index];
+        if(character == null){
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            passed = false
+        }else if(character === characterSpan.innerText){
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+            correct += 1;
+            correctInput.textContent = correct;
+            
+        }else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            passed = false
+        }
+
+    })
+    userInput.innerText = null
+    if(passed){
+        clearInterval(intervalId)
+        results.textContent = `Congradulations. You typed ${showLength.textContent} letters in ${timerSelector.tex} seconds`
+    }
+    
+})
+
+let timer = 0;
+let intervalId;
+let total = 0;
+timerBtnStart.addEventListener('click', () => {
+  
+    intervalId = setInterval(() => {
+        timer += 1;
+        timerSelector.textContent = timer;
+    }, 1000)
+})
+
+btnPause.addEventListener('click', ()=> {
+    clearInterval(intervalId)
+    let total = btnPause.textContent
+})
+
+next.addEventListener('click', () => {
+    displayQuotes()
+})
+displayQuotes()
+
